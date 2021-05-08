6eb4545f5a6a2ce27eb5fc48ee42e16e510e2183
Labbaek ratul <labbaekratul@gmail.com>
2021-05-08 09:16:23 -0700

    first commit

---
+++ src/AppBody.js
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from "react";
+import RoomIcon from "@material-ui/icons/Room";
+import "./Styles/AppBody.css";
+import clearstatus from "./images/clear.png";
+import { RiTempHotLine } from "react-icons/ri";
+import { FiSunrise, FiSunset } from "react-icons/fi";
+import { BsDroplet, BsArrowUp } from "react-icons/bs";
+import { GiSpeedometer } from "react-icons/gi";
+import WeaklyChart from "./WeaklyChart ";
+import axios from "axios";
+import rainstatus from "./images/rain.png";
+import couldstatus from "./images/cloud.png";
+import hezestatus from "./images/19.png";
+
+function AppBody() {
+  const [country, setCountry] = useState("");
+  const [search, setSearch] = useState("");
+  const [data, setData] = useState();
+
+  const city = "Dhaka";
+  const time = new Date();
+
+  const month = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  const day = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const getCountyName = (e) => {
+    e.preventDefault();
+    setSearch(country);
+    setCountry("");
+  };
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/weather?q=${
+          search ? search : city
+        }&appid=716536c610d959ddf3e0456b12b9bbf0`
+      )
+      .then((res) => {
+        setData(res.data);
+      });
+  }, [search]);
+
+  return (
+    <div className="appbody">
+      <div className="container-fluid">
+        <div className="row">
+          <form className="col-12 appbody__inputCol" onSubmit={getCountyName}>
+            <input
+              className="appbody__countrySearch"
+              type="text"
+              placeholder="Write Your City Name.. ."
+              value={country}
+              onChange={(e) => setCountry(e.target.value)}
+            />
+          </form>
+          <div className="appbody__cityName col-12">
+            <RoomIcon className="county-mapicon" />
+            <span className="appbody__city">{data?.name},</span>
+            <span className="appbody__contry">{data?.sys.country}</span>
+          </div>
+          <div className="row weatherstatus_row">
+            <div className="col-xl-4 col-lg-6 col-md-6 col-12 date__info">
+              <div>
+                <p className="mb-1">
+                  {month[time.getMonth()]}, {time.getDate()}
+                </p>
+                <small>
+                  {day[time.getDay()]}, {time.getFullYear()}
+                </small>
+              </div>
+              <div className="tempInfo">
+                <div className="tempInfo__icon">
+                  <div className="tempIconAndStatus">
+                    {Math.round(data?.main.temp - 275.15) >= 25 ? (
+                      <RiTempHotLine
+                        className="temp_icon1"
+                        style={{ color: "red" }}
+                      />
+                    ) : Math.round(data?.main.temp - 275.15) <= 25 >= 10 ? (
+                      <RiTempHotLine
+                        className="temp_icon1"
+                        style={{ color: "yellow" }}
+                      />
+                    ) : Math.round(data?.main.temp - 275.15) <= 10 ? (
+                      <RiTempHotLine
+                        className="temp_icon1"
+                        style={{ color: "white" }}
+                      />
+                    ) : (
+                      <RiTempHotLine
+                        className="temp_icon1"
+                        style={{ color: "yellow" }}
+                      />
+                    )}
+                    <p className="temp_degree">
+                      {Math.round(data?.main.temp - 275.15)} <sup>0</sup>C
+                    </p>
+                  </div>
+                  <p>
+                    Feels like {Math.round(data?.main.feels_like - 275.15)}
+                    <sup> 0</sup>C
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-xl-4 col-lg-6 col-md-6 col-12  status__imgCol">
+              {data?.weather[0].main == "Rain" ? (
+                <img src={rainstatus} className="status__img" alt="img1" />
+              ) : data?.weather[0].main == "Clouds" ? (
+                <img src={couldstatus} className="status__img" alt="img1" />
+              ) : data?.weather[0].main == "Clear" ? (
+                <img src={clearstatus} className="status__img" alt="img1" />
+              ) : (
+                <img src={hezestatus} className="status__img" alt="img1" />
+              )}
+              <h2 className="status__img_text">{data?.weather[0].main}</h2>
+            </div>
+            <div className="col-xl-4 col-lg-6 col-md-6 col-12 weatherDeatilnInfo__col">
+              <div>
+                <p className="singleLineStatus">
+                  <FiSunrise className="sunrise_icon" /> Max Temp :
+                  <span className="sunset_time">
+                    {Math.round(data?.main.temp_max - 275.15)} <sup>0</sup>C
+                  </span>
+                </p>
+                <p className="singleLineStatus">
+                  <FiSunset className="sunrise_icon" /> Min Temp :
+                  <span className="sunset_time">
+                    {Math.round(data?.main.temp_min - 275.15)} <sup>0</sup>C
+                  </span>
+                </p>
+                <p className="singleLineStatus">
+                  <BsDroplet className="sunrise_icon" /> Humidity :{" "}
+                  {data?.main.humidity}%
+                </p>
+                <p className="singleLineStatus">
+                  <GiSpeedometer className="sunrise_icon" /> Wind-speed :{" "}
+                  {data?.wind.speed} m/s
+                </p>
+                <p className="singleLineStatus">
+                  <BsArrowUp className="sunrise_icon" /> Pressure :{" "}
+                  {data?.main.pressure} mb
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <WeaklyChart search={search} />
+      </div>
+    </div>
+  );
+}
+
+export default AppBody;
---
+++ src/WeaklyChart .js
@@ -0,0 +1,169 @@
+import React, { useState, useEffect } from "react";
+import "./Styles/WeaklyChart.css";
+import sun from "./images/26.png";
+import cloud from "./images/23.png";
+import cloud2 from "./images/22.png";
+import snow from "./images/36.png";
+import light from "./images/30.png";
+import moon from "./images/19.png";
+import Carousel from "react-elastic-carousel";
+import axios from "axios";
+import rainstatus from "./images/rain.png";
+import couldstatus from "./images/cloud.png";
+import hezestatus from "./images/19.png";
+import clearstatus from "./images/clear.png";
+
+const breakPoints = [
+  { width: 1, itemsToShow: 1 },
+  { width: 550, itemsToShow: 2 },
+  { width: 768, itemsToShow: 3 },
+  { width: 1100, itemsToShow: 4 },
+];
+
+function WeaklyChart({ search }) {
+  const [cardData, setCardData] = useState("");
+  const cityName = "Dhaka";
+
+  useEffect(() => {
+    axios
+      .get(
+        `http://api.openweathermap.org/data/2.5/forecast?q=${
+          search ? search : cityName
+        }&appid=716536c610d959ddf3e0456b12b9bbf0`
+      )
+      .then((res) => {
+        setCardData(res.data.list);
+      });
+  }, [search]);
+
+  console.log(cardData[0]?.weather[0].main);
+
+  const date = new Date(cardData[0]?.dt_txt);
+  const date1 = new Date(cardData[5]?.dt_txt);
+  const date2 = new Date(cardData[15]?.dt_txt);
+  const date3 = new Date(cardData[22]?.dt_txt);
+  const date4 = new Date(cardData[31]?.dt_txt);
+  const date5 = new Date(cardData[38]?.dt_txt);
+  const date6 = new Date(cardData[39]?.dt_txt);
+
+  return (
+    <div className="weaklychart">
+      <div className="container weaklychart__conatainer">
+        <Carousel breakPoints={breakPoints} className="row weaklychart__row">
+          <div className="col-12 col-md-2 weaklychart__card">
+            {cardData[0]?.weather[0].main === "Rain" ? (
+              <img src={rainstatus} className="sun_img" alt="img1" />
+            ) : cardData[0]?.weather[0].main === "Clouds" ? (
+              <img src={couldstatus} className="sun_img" alt="img1" />
+            ) : cardData[0]?.weather[0].main === "Clear" ? (
+              <img src={clearstatus} className="sun_img" alt="img1" />
+            ) : (
+              <img src={hezestatus} className="sun_img" alt="img1" />
+            )}
+            <p className="weaklychart__temp">
+              Temp : {Math.round(cardData[0]?.main.temp - 275.15)} <sup>0</sup>C
+            </p>
+            <p>Date : {date.toLocaleDateString()}</p>
+          </div>
+          <div className="col-12 col-md-2 weaklychart__card">
+            {cardData[5]?.weather[0].main === "Rain" ? (
+              <img src={rainstatus} className="sun_img" alt="img1" />
+            ) : cardData[5]?.weather[0].main === "Clouds" ? (
+              <img src={couldstatus} className="sun_img" alt="img1" />
+            ) : cardData[5]?.weather[0].main === "Clear" ? (
+              <img src={clearstatus} className="sun_img" alt="img1" />
+            ) : (
+              <img src={hezestatus} className="sun_img" alt="img1" />
+            )}
+            <p className="weaklychart__temp">
+              Temp : {Math.round(cardData[5]?.main.temp - 275.15)} <sup>0</sup>C
+            </p>
+            <p>Date : {date1.toLocaleDateString()}</p>
+          </div>
+          <div className="col-12 col-md-2  weaklychart__card">
+            {cardData[15]?.weather[0].main === "Rain" ? (
+              <img src={rainstatus} className="sun_img" alt="img1" />
+            ) : cardData[15]?.weather[0].main === "Clouds" ? (
+              <img src={couldstatus} className="sun_img" alt="img1" />
+            ) : cardData[15]?.weather[0].main === "Clear" ? (
+              <img src={clearstatus} className="sun_img" alt="img1" />
+            ) : (
+              <img src={hezestatus} className="sun_img" alt="img1" />
+            )}
+            <p className="weaklychart__temp">
+              Temp : {Math.round(cardData[15]?.main.temp - 275.15)} <sup>0</sup>
+              C
+            </p>
+            <p>Date : {date2.toLocaleDateString()}</p>
+          </div>
+          <div className="col-12 col-md-2 weaklychart__card">
+            {cardData[22]?.weather[0].main === "Rain" ? (
+              <img src={rainstatus} className="sun_img" alt="img1" />
+            ) : cardData[22]?.weather[0].main === "Clouds" ? (
+              <img src={couldstatus} className="sun_img" alt="img1" />
+            ) : cardData[22]?.weather[0].main === "Clear" ? (
+              <img src={clearstatus} className="sun_img" alt="img1" />
+            ) : (
+              <img src={hezestatus} className="sun_img" alt="img1" />
+            )}
+            <p className="weaklychart__temp">
+              Temp : {Math.round(cardData[22]?.main.temp - 275.15)} <sup>0</sup>
+              C
+            </p>
+            <p>Date : {date3.toLocaleDateString()}</p>
+          </div>
+          <div className="col-12 col-md-2 weaklychart__card">
+            {cardData[31]?.weather[0].main === "Rain" ? (
+              <img src={rainstatus} className="sun_img" alt="img1" />
+            ) : cardData[31]?.weather[0].main === "Clouds" ? (
+              <img src={couldstatus} className="sun_img" alt="img1" />
+            ) : cardData[31]?.weather[0].main === "Clear" ? (
+              <img src={clearstatus} className="sun_img" alt="img1" />
+            ) : (
+              <img src={hezestatus} className="sun_img" alt="img1" />
+            )}
+            <p className="weaklychart__temp">
+              Temp : {Math.round(cardData[31]?.main.temp - 275.15)} <sup>0</sup>
+              C
+            </p>
+            <p>Date : {date4.toLocaleDateString()}</p>
+          </div>
+          <div className="col-12 col-md-2 weaklychart__card">
+            {cardData[38]?.weather[0].main === "Rain" ? (
+              <img src={rainstatus} className="sun_img" alt="img1" />
+            ) : cardData[38]?.weather[0].main === "Clouds" ? (
+              <img src={couldstatus} className="sun_img" alt="img1" />
+            ) : cardData[38]?.weather[0].main === "Clear" ? (
+              <img src={clearstatus} className="sun_img" alt="img1" />
+            ) : (
+              <img src={hezestatus} className="sun_img" alt="img1" />
+            )}
+            <p className="weaklychart__temp">
+              Temp : {Math.round(cardData[38]?.main.temp - 275.15)} <sup>0</sup>
+              C
+            </p>
+            <p>Date : {date5.toLocaleDateString()}</p>
+          </div>
+          <div className="col-12 col-md-2 weaklychart__card">
+            {cardData[39]?.weather[0].main === "Rain" ? (
+              <img src={rainstatus} className="sun_img" alt="img1" />
+            ) : cardData[39]?.weather[0].main === "Clouds" ? (
+              <img src={couldstatus} className="sun_img" alt="img1" />
+            ) : cardData[39]?.weather[0].main === "Clear" ? (
+              <img src={clearstatus} className="sun_img" alt="img1" />
+            ) : (
+              <img src={hezestatus} className="sun_img" alt="img1" />
+            )}
+            <p className="weaklychart__temp">
+              Temp : {Math.round(cardData[39]?.main.temp - 275.15)} <sup>0</sup>
+              C
+            </p>
+            <p>Date : {date6.toLocaleDateString()}</p>
+          </div>
+        </Carousel>
+      </div>
+    </div>
+  );
+}
+
+export default WeaklyChart;
